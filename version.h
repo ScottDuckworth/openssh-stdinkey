b48324b185154688b9321f5d79593caf95bded3f
Scott Duckworth <sduckwo@clemson.edu>
2014-03-20 15:01:41 -0400

    openssh-6.4p1

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
-/* $OpenBSD: version.h,v 1.66 2013/02/10 21:19:34 markus Exp $ */
+/* $OpenBSD: version.h,v 1.68 2013/11/08 01:38:11 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_6.2"
+#define SSH_VERSION	"OpenSSH_6.4"
 
-#define SSH_PORTABLE	"p2"
+#define SSH_PORTABLE	"p1"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE
